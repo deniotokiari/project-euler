4f4dd950bef0b6e13007f95e8ce8f7c7a84bab68
deniotokiari <deniotokiari@gmail.com>
2018-09-26 12:21:22 +0300

    Create problem_5.clj

---
+++ problems/problem_5.clj
@@ -0,0 +1,13 @@
+;; Smallest multiple
+;; =================
+;; 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+;; What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+;; =================
+
+(defn evenly-divisible? [number start end]
+  (empty? (filter pos? (map #(rem number %) (range start (inc end)))))
+)
+
+(let [start 1 end 20]
+  (first (drop-while #(not (evenly-divisible? % start end)) (drop end (range))))
+)
