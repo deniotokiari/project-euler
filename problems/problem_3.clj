0ae7b2d36e35b26a691c36c6ccc55c789cd705e5
deniotokiari <deniotokiari@gmail.com>
2018-09-26 10:52:51 +0300

    Create problem_3.clj

---
+++ problems/problem_3.clj
@@ -0,0 +1,24 @@
+;; Largest prime factor
+;; ====================
+;; The prime factors of 13195 are 5, 7, 13 and 29.
+;; What is the largest prime factor of the number 600851475143 ?
+;; ====================
+
+(defn prime? [x] 
+  (empty? 
+    (filter zero? 
+      (map #(mod x %) 
+        (range 2 (inc (int (Math/floor (Math/sqrt x)))))
+      )
+    )
+  )
+)
+
+
+(let [n 600851475143]
+  (apply max 
+    (filter #(and (zero? (mod n %)) (prime? %)) 
+      (reverse (sort (range 2 (inc (int (Math/floor (Math/sqrt n)))))))
+    )
+  )
+)
